2d4dec76163ccc89088644de665147f1c88b7030
n1ved <nivedrsalini@gmail.com>
2023-05-15 20:36:49 +0530

    d1 html elemets completed

---
+++ src/pages/home/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import heroimg from './../../images/heroimg.jpg'
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+            <div className='nav'>
+                <div className='nav-left'>
+                    <h1>NearPG</h1>
+                </div>
+                <div className='nav-right'>
+                    <a href="/login">
+                        login
+                    </a>
+                </div>
+            </div>
+            <div className='hero-cont'>
+                <div className='hero-left'>
+                    <h1>Discover your dream PG  today</h1>
+                    <button>Join Today</button>
+                </div>
+                <div className='hero-right'>
+                    <img src={heroimg} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
