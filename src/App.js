2c20b78a41b9cc237d8526dfa054034bd646ec90
n1ved <nivedrsalini@gmail.com>
2023-05-16 19:26:05 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import Hero from './pages/home/Hero'
+import Login from './pages/login/login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Hero />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Hero />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
